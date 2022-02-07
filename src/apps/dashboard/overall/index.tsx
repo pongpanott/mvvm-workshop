7d3486e53e74cc46dd671f446b4f53846d6914d6
Pongpanot Tunkrongsin <fhyptr@gmail.com>
2022-02-08 03:09:19 +0700

    hard mode

--- src/apps/dashboard/overall/index.tsx
+++ src/apps/dashboard/overall/index.tsx
@@ -5,26 +5,26 @@ import { TableRow } from "./components/tableRow";
 import useViewModel from "./useViewModel";
 
 export const OverallPage = () => {
-  const { data, errorsRows, onRemoveData, onDuplicateData, toggle, setToggle } =
-    useViewModel();
+	const { data, errorsRows, onRemoveData, onDuplicateData, toggle, setToggle } =
+		useViewModel();
 
-  return (
-    <div className=" p-8 pt-[72px] mx-auto">
-      <Toggle enabled={toggle} setEnabled={() => setToggle(!toggle)} />
-      <Table>
-        <TableColumn />
-        <TableBody>
-          {data.map((entry, index) => (
-            <TableRow
-              data={entry}
-              index={index}
-              onRemove={() => onRemoveData(index)}
-              onDuplicateData={onDuplicateData}
-              error={errorsRows.includes(index)}
-            />
-          ))}
-        </TableBody>
-      </Table>
-    </div>
-  );
+	return (
+		<div className=" p-8 pt-[72px] mx-auto">
+			<Toggle enabled={toggle} setEnabled={() => setToggle(!toggle)} />
+			<Table>
+				<TableColumn />
+				<TableBody>
+					{data.map((entry, index) => (
+						<TableRow
+							data={entry}
+							index={index}
+							onRemove={() => onRemoveData(index)}
+							onDuplicateData={() => onDuplicateData(index)}
+							error={errorsRows.includes(index)}
+						/>
+					))}
+				</TableBody>
+			</Table>
+		</div>
+	);
 };
--- src/apps/dashboard/overall/useViewModel.tsx
+++ src/apps/dashboard/overall/useViewModel.tsx
@@ -3,320 +3,397 @@ import { useState, useEffect } from "react";
 import { getData } from "../../../service/getData";
 
 interface IObjectData {
-  index: number;
-  data: IBaseData;
+	index: number;
+	data: IBaseData;
 }
 
 interface IObjectError {
-  index: number;
-  pos: number;
+	index: number;
+	pos: number;
 }
 
 const useViewModel = () => {
-  const [data, setData] = useState<IBaseData[]>([]);
-  const [holdData, setHoldData] = useState<
-    { index: number; data: IBaseData }[]
-  >([]);
-  const [errorsRows, setErrorsRows] = useState([0, 2]);
-  const [errorPos, setErrorPos] = useState<{ index: number; pos: number }[]>(
-    []
-  );
-  const [toggle, setToggle] = useState(false);
+	const [data, setData] = useState<IBaseData[]>([]);
+	const [holdData, setHoldData] = useState<
+		{ index: number; data: IBaseData }[]
+	>([]);
+	const [errorsRows, setErrorsRows] = useState([0, 2]);
+	const [errorPos, setErrorPos] = useState<{ index: number; pos: number }[]>(
+		[]
+	);
+	const [toggle, setToggle] = useState(false);
 
-  const createHoldDataObject = (curData: IBaseData[]) => {
-    let dataObject: IObjectData[] = [];
+	const removeData = (
+		index: number,
+		curData: { index: number; data: IBaseData }[]
+	) => {
+		curData.splice(index, 1);
 
-    curData.forEach((item, index) => {
-      let newObject = { index, data: item };
+		let lowerData: IObjectData[] = [];
+		let upperData: IObjectData[] = [];
 
-      dataObject.push(newObject);
-    });
+		curData.forEach((data) => {
+			if (data.index < index) {
+				lowerData.push(data);
+			}
+			if (data.index > index) {
+				upperData.push(data);
+			}
+		});
 
-    return dataObject;
-  };
+		let newUpperData = upperData.map((item) => {
+			const { index, data } = item;
 
-  const removeData = (
-    index: number,
-    curData: { index: number; data: IBaseData }[]
-  ) => {
-    curData.splice(index, 1);
+			return { index: index - 1, data };
+		});
 
-    let lowerData: IObjectData[] = [];
-    let upperData: IObjectData[] = [];
+		let sumData = [...lowerData, ...newUpperData];
 
-    curData.forEach((data) => {
-      if (data.index < index) {
-        lowerData.push(data);
-      }
-      if (data.index > index) {
-        upperData.push(data);
-      }
-    });
+		return sumData;
+	};
 
-    let newUpperData = upperData.map((item) => {
-      const { index, data } = item;
+	const removeError = (index: number, curError: IObjectError[]) => {
+		let lowerError: IObjectError[] = [];
+		let upperError: IObjectError[] = [];
 
-      return { index: index - 1, data };
-    });
+		curError.forEach((item) => {
+			if (item.pos < index) {
+				lowerError.push(item);
+			}
+			if (item.pos > index) {
+				upperError.push(item);
+			}
+		});
 
-    let sumData = [...lowerData, ...newUpperData];
+		let newUpperError = upperError.map((item) => {
+			const { index, pos } = item;
 
-    return sumData;
-  };
+			return { index: index - 1, pos: pos - 1 };
+		});
 
-  const removeError = (index: number, curError: IObjectError[]) => {
-    let lowerError: IObjectError[] = [];
-    let upperError: IObjectError[] = [];
+		let sumError = [...lowerError, ...newUpperError];
 
-    let spliceErr = curError.filter((err) => {
-      if (err.pos !== index) {
-        return err;
-      }
-    });
+		return sumError;
+	};
 
-    spliceErr.forEach((item) => {
-      if (item.pos < index) {
-        lowerError.push(item);
-      }
-      if (item.pos > index) {
-        upperError.push(item);
-      }
-    });
+	const removeToggleData = (index: number, curData: IBaseData[]) => {
+		let lowerData: IObjectData[] = [];
+		let upperData: IObjectData[] = [];
 
-    let newUpperError = upperError.map((item) => {
-      const { index, pos } = item;
+		let removed = curData.filter((data, dataIndex) => dataIndex === index);
+		let getLeftData = holdData.filter((val) => !removed.includes(val.data));
 
-      return { index: index - 1, pos: pos - 1 };
-    });
+		getLeftData.forEach((data) => {
+			if (data.index <= index) {
+				lowerData.push(data);
+			}
+			if (data.index > index) {
+				upperData.push(data);
+			}
+		});
 
-    let sumError = [...lowerError, ...newUpperError];
+		let newUpperData = upperData.map((item) => {
+			const { index, data } = item;
 
-    return sumError;
-  };
+			return { index: index - 1, data };
+		});
 
-  const removeToggleData = (index: number, curData: IBaseData[]) => {
-    let lowerData: IObjectData[] = [];
-    let upperData: IObjectData[] = [];
+		let sumData = [...lowerData, ...newUpperData];
 
-    let removed = curData.filter((data) => data.id === index);
-    let getLeftData = holdData.filter((val) => !removed.includes(val.data));
+		return sumData;
+	};
 
-    getLeftData.forEach((data) => {
-      if (data.index < index) {
-        lowerData.push(data);
-      }
-      if (data.index > index) {
-        upperData.push(data);
-      }
-    });
+	const onRemoveData = (index: number) => {
+		if (toggle) {
+			let curData = data;
+			let curError = errorPos;
 
-    let newUpperData = upperData.map((item) => {
-      const { index, data } = item;
+			let removed = curData.filter((data, dataIndex) => dataIndex === index);
+			let leftData = removeToggleData(index, curData);
+			let newCurData = curData.filter((data) => !removed.includes(data));
 
-      return { index: index - 1, data };
-    });
+			let leftError = removeError(index, curError);
+			let curLeftError = leftError.map((err) => {
+				if (err.pos === 0) {
+					return err.pos;
+				} else {
+					return err.pos - 1;
+				}
+			});
 
-    let sumData = [...lowerData, ...newUpperData];
+			setData(newCurData);
+			setHoldData(leftData);
 
-    return sumData;
-  };
+			setErrorPos(leftError);
+			setErrorsRows(curLeftError);
+		}
+		if (!toggle) {
+			let curData = holdData;
+			let curError = errorPos;
 
-  const onRemoveData = (index: number) => {
-    if (toggle) {
-      let curData = data;
-      let curError = errorPos;
+			let leftData = removeData(index, curData);
+			let curLeftData = leftData.map((data) => data.data);
 
-      let removed = curData.filter((data) => data.id === index);
-      let leftData = removeToggleData(index, curData);
-      let newCurData = curData.filter((data) => !removed.includes(data));
+			setHoldData(leftData);
+			setData(curLeftData);
 
-      let leftError = removeError(index, curError);
-      let curLeftError = leftError.map((err) => err.pos - 1);
+			let leftError = removeError(index, curError);
+			let curLeftError = leftError.map((err) => err.pos);
 
-      setData(newCurData);
-      setHoldData(leftData);
+			setErrorPos(leftError);
+			setErrorsRows(curLeftError);
+		}
+		return null;
+	};
 
-      setErrorPos(leftError);
-      setErrorsRows(curLeftError);
-    }
-    if (!toggle) {
-      let curData = holdData;
-      let curError = errorPos;
+	const duplicateData = (
+		index: number,
+		duplicate: IBaseData[],
+		curData: IBaseData[]
+	) => {
+		let lowerData: IBaseData[] = [];
+		let upperData: IBaseData[] = [];
 
-      let leftData = removeData(index, curData);
-      let curLeftData = leftData.map((data) => data.data);
+		curData.forEach((data) => {
+			if (data.id < index) {
+				lowerData.push(data);
+			}
+			if (data.id >= index) {
+				upperData.push(data);
+			}
+		});
 
-      setHoldData(leftData);
-      setData(curLeftData);
+		let createDuplicate = duplicate.map((item) => {
+			let newDup = { ...item, id: item.id + 1 };
 
-      let leftError = removeError(index, curError);
-      let curLeftError = leftError.map((err) => err.pos);
+			return newDup;
+		});
 
-      setErrorPos(leftError);
-      setErrorsRows(curLeftError);
-    }
-    return null;
-  };
+		let newUpperData = upperData.map((item) => {
+			let newUpper = { ...item, id: item.id + 1 };
+			return newUpper;
+		});
 
-  const onDuplicateData = () => {
-    return false;
-  };
+		let sumData = [...lowerData, createDuplicate[0], ...newUpperData];
 
-  const orderError = (arrayOfError: number[], errorData: IBaseData[]) => {
-    let array: number[] = [];
+		return sumData;
+	};
 
-    for (let index = 0; index < errorData.length; index++) {
-      array.push(index);
-    }
+	const duplicateError = (index: number, curError: IObjectError[]) => {
+		let lowerError: IObjectError[] = [];
+		let upperError: IObjectError[] = [];
 
-    let errorPosArray: { index: number; pos: number }[] = [];
-    arrayOfError.forEach((error, index) => {
-      let newErrorObj = { index, pos: error };
+		let findError = curError.filter((err) => err.pos === index);
 
-      errorPosArray.push(newErrorObj);
-    });
+		let newError = findError.map((item) => {
+			let newObjError = { index: item.index + 1, pos: item.pos + 1 };
 
-    return errorPosArray;
-  };
+			return newObjError;
+		});
 
-  const toggleTrue = () => {
-    let getErrorData = holdData.filter((val) => errorsRows.includes(val.index));
-    let errorData = getErrorData.map((item) => item.data);
+		curError.forEach((err) => {
+			if (err.pos <= index) {
+				lowerError.push(err);
+			}
+			if (err.pos > index) {
+				upperError.push(err);
+			}
+		});
 
-    setData(errorData);
+		let newUpperError = upperError.map((item) => {
+			let newObjError = { index: item.index + 1, pos: item.pos + 1 };
 
-    let newErrorPos = orderError(errorsRows, errorData);
-    let newRowsErrors = newErrorPos.map((item) => item.index);
+			return newObjError;
+		});
 
-    setErrorPos(newErrorPos);
-    setErrorsRows(newRowsErrors);
+		let sumError = [...lowerError, ...newError, ...newUpperError];
 
-    return false;
-  };
+		return sumError;
+	};
 
-  const toggleFalse = () => {
-    let setDataBack = holdData.map((item) => item.data);
-    setData(setDataBack);
+	const duplicateToggleError = (index: number, curError: IObjectError[]) => {
+		let lowerError: IObjectError[] = [];
+		let upperError: IObjectError[] = [];
 
-    let setErrorPosBack = errorPos.map((error) => error.pos);
-    setErrorsRows(setErrorPosBack);
+		let findError = curError.filter((err) => err.index === index);
 
-    return false;
-  };
+		let newError = findError.map((item) => {
+			let newObjError = { index: item.index + 1, pos: item.pos + 1 };
 
-  useEffect(() => {
-    if (holdData.length === 0) {
-      console.log("fetch");
-      getData().then((res) => {
-        let dataObject: IObjectData[] = [];
-        let errorObject: IObjectError[] = [];
+			return newObjError;
+		});
 
-        res.forEach((item, index) => {
-          let newObject = { index, data: item };
+		curError.forEach((err) => {
+			if (err.index <= index) {
+				lowerError.push(err);
+			}
+			if (err.index > index) {
+				upperError.push(err);
+			}
+		});
 
-          dataObject.push(newObject);
-        });
+		let newUpperError = upperError.map((item) => {
+			let newObjError = { index: item.index + 1, pos: item.pos + 1 };
 
-        errorsRows.forEach((item, index) => {
-          let newErrorObj = { index, pos: item };
+			return newObjError;
+		});
 
-          errorObject.push(newErrorObj);
-        });
+		let sumError = [...lowerError, ...newError, ...newUpperError];
 
-        setData(res);
-        setHoldData(dataObject);
-        setErrorPos(errorObject);
-      });
-    }
-    if (holdData.length !== 0) {
-      if (toggle) {
-        toggleTrue();
-      }
-      if (!toggle) {
-        toggleFalse();
-      }
-    }
-  }, [toggle]);
+		return sumError;
+	};
 
-  return {
-    data,
-    setData,
-    errorsRows,
-    onRemoveData,
-    onDuplicateData,
-    toggle,
-    setToggle,
-  };
+	const onDuplicateData = (index: number) => {
+		if (toggle) {
+			let curData = data;
+			let curError = errorPos;
+			let newHoldData: IObjectData[] = [];
+			let lowerHoldData: IObjectData[] = [];
+			let upperHoldData: IObjectData[] = [];
+
+			let duplicate = curData.filter((data, dataIndex) => dataIndex === index);
+			let newData = duplicateData(index, duplicate, curData);
+
+			holdData.forEach((item) => {
+				if (item.index <= index) {
+					lowerHoldData.push(item);
+				}
+				if (item.index > index) {
+					upperHoldData.push(item);
+				}
+			});
+
+			let duplicateHoldData = { index: index + 1, data: duplicate[0] };
+			let newUpperHoldData = upperHoldData.map((item) => {
+				let newObj = { index: item.index + 1, data: item.data };
+
+				return newObj;
+			});
+
+			let sumHoldData = [
+				...lowerHoldData,
+				duplicateHoldData,
+				...newUpperHoldData,
+			];
+
+			setData(newData);
+			setHoldData(sumHoldData);
+
+			let newErrorPos = duplicateToggleError(index, curError);
+			let newError = newErrorPos.map((item) => item.index);
+
+			setErrorPos(newErrorPos);
+			setErrorsRows(newError);
+		}
+		if (!toggle) {
+			let curData = data;
+			let curError = errorPos;
+			let newHoldData: IObjectData[] = [];
+
+			let duplicate = curData.filter((data, dataIndex) => dataIndex === index);
+			let newData = duplicateData(index, duplicate, curData);
+			newData.forEach((item, index) => {
+				let newObject = { index, data: item };
+
+				newHoldData.push(newObject);
+			});
+
+			setData(newData);
+			setHoldData(newHoldData);
+
+			let newErrorPos = duplicateError(index, curError);
+			let newError = newErrorPos.map((item) => item.pos);
+
+			setErrorPos(newErrorPos);
+			setErrorsRows(newError);
+		}
+	};
+
+	const orderError = (arrayOfError: number[], errorData: IBaseData[]) => {
+		let array: number[] = [];
+
+		for (let index = 0; index < errorData.length; index++) {
+			array.push(index);
+		}
+
+		let errorPosArray: { index: number; pos: number }[] = [];
+		arrayOfError.forEach((error, index) => {
+			let newErrorObj = { index, pos: error };
+
+			errorPosArray.push(newErrorObj);
+		});
+
+		return errorPosArray;
+	};
+
+	const toggleTrue = () => {
+		let getErrorData = holdData.filter((val) => errorsRows.includes(val.index));
+		let errorData = getErrorData.map((item) => item.data);
+
+		setData(errorData);
+
+		let newErrorPos = orderError(errorsRows, errorData);
+		let newRowsErrors = newErrorPos.map((item) => item.index);
+
+		setErrorPos(newErrorPos);
+		setErrorsRows(newRowsErrors);
+
+		return false;
+	};
+
+	const toggleFalse = () => {
+		let setDataBack = holdData.map((item) => item.data);
+		setData(setDataBack);
+
+		let setErrorPosBack = errorPos.map((error) => error.pos);
+		setErrorsRows(setErrorPosBack);
+
+		return false;
+	};
+
+	useEffect(() => {
+		if (holdData.length === 0) {
+			console.log("fetch");
+			getData().then((res) => {
+				let dataObject: IObjectData[] = [];
+				let errorObject: IObjectError[] = [];
+
+				res.forEach((item, index) => {
+					let newObject = { index, data: item };
+
+					dataObject.push(newObject);
+				});
+
+				errorsRows.forEach((item, index) => {
+					let newErrorObj = { index, pos: item };
+
+					errorObject.push(newErrorObj);
+				});
+
+				setData(res);
+				setHoldData(dataObject);
+				setErrorPos(errorObject);
+			});
+		}
+		if (holdData.length !== 0) {
+			if (toggle) {
+				toggleTrue();
+			}
+			if (!toggle) {
+				toggleFalse();
+			}
+		}
+	}, [toggle]);
+
+	return {
+		data,
+		setData,
+		errorsRows,
+		onRemoveData,
+		onDuplicateData,
+		toggle,
+		setToggle,
+	};
 };
 
 export default useViewModel;
-
-// curData.splice(index, 1);
-// let errorPos = arrayDiff(errorsRows);
-// errorPos.forEach((item, Itemindex) => {
-//   if (Itemindex < index) {
-//     lowerErr.push(item);
-//   }
-//   if (Itemindex > index) {
-//     higherErr.push(item);
-//   }
-// });
-// let sumErrorPos = [...lowerErr, ...higherErr];
-// let newErrorPos = sumErrorPos.map((i) => {
-//   if (i > 0) {
-//     return i - 1;
-//   } else {
-//     return i;
-//   }
-// });
-
-// let newErrorsRows = newErrorPos.filter((i) => i >= 0);
-
-// const removeToggleError = (index: number, curError: IObjectError[]) => {
-//   let lowerError: IObjectError[] = [];
-//   let upperError: IObjectError[] = [];
-
-//   let spliceErr = curError.filter((err) => {
-//     if (err.pos !== index) {
-//       return err;
-//     }
-//   });
-
-//   spliceErr.forEach((item) => {
-//     if (item.pos < index) {
-//       lowerError.push(item);
-//     }
-//     if (item.pos > index) {
-//       upperError.push(item);
-//     }
-//   });
-
-//   let newUpperError = upperError.map((item) => {
-//     const { index, pos } = item;
-
-//     return { index: index - 1, pos: pos - 1 };
-//   });
-
-//   let sumError = [...lowerError, ...newUpperError];
-
-//   return sumError;
-// };
-
-// const arrayDiff = (arrayOfError: number[], data: IBaseData[]) => {
-//   let newerrorPos: number[] = [];
-//   let array: number[] = [];
-
-//   for (let index = 0; index <= data.length; index++) {
-//     array.push(index);
-//   }
-
-//   array.filter(function (i) {
-//     if (arrayOfError.indexOf(i) < 0) {
-//       newerrorPos.push(-1);
-//     } else {
-//       newerrorPos.push(array.indexOf(i));
-//     }
-//   });
-
-//   return newerrorPos;
-// };
